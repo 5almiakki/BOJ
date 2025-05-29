de8fc4a6fe96fd2613e523a651ca7690c7183ec2
KimOneChan <99474425+5almiakki@users.noreply.github.com>
2025-05-30 01:00:54 +0900

    [level 2] Title: 전화번호 목록, Time: 389.01 ms, Memory: 239 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,10 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phoneBook) {
-        Arrays.sort(phoneBook);
-        for (int i = 0; i < phoneBook.length - 1; i++) {
-            if (phoneBook[i + 1].startsWith(phoneBook[i])) {
-                return false;
+        Set<String> phoneNums = new HashSet<>();
+        for (String phoneNum : phoneBook) {
+            phoneNums.add(phoneNum);
+        }
+        for (String phoneNum : phoneNums) {
+            for (int i = 1; i < phoneNum.length(); i++) {
+                if (phoneNums.contains(phoneNum.substring(0, i))) {
+                    return false;
+                }
             }
         }
         return true;
